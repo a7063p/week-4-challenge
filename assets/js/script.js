d5d57432bfcbaccfc8f8f05e38e326ecf42a430a
andrew <ap@ampiusa.com>
2021-01-04 12:55:51 -0800

    Dynamic Functions set for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var answerInputEl = document.querySelector("#answers");
-var questionInputEl = document.querySelector("#questions")
-var buttonEl = document.querySelector("#question2-id", "#question1-id");
+var questionGroupEl = document.querySelector("#questions-group");
+var questionInputEl = document.querySelector("#questions");
 
-var questionArray = [];
+var buttonEl = document.querySelector("#question2-id", "#question1-id");
 
 
 
@@ -11,37 +11,104 @@ var question1 = function () {
         question: "What is JavaScript?",
         answer: ["program", "coffee", "computer", "Dont Know"]   
     }
-    questionArray.push(testObj.question);
-    console.log(questionArray);
+      console.log(testObj);
+      createQuestionEl(testObj)
+     
 
-    //testFunc()
-    
+}     
+
+
+    var createQuestionEl = function(testObj) {
+
+        var groupEl = document.createElement('div');
+        groupEl.className = "question-wrapper"
+        groupEl.id = "#test-id"
+        // groupEl.innerHTML = "<h2 class='question-title'>" + testObj.question + "</h2>";
+        
+        questionGroupEl.appendChild(groupEl)
+
+        var questionTitleEl = document.createElement('h2')
+        groupEl.appendChild(questionTitleEl)
+
+        var questions = document.createElement('p');
+        questions.textContent = testObj.question
+        questionTitleEl.appendChild(questions)
+
+        var answerEl = document.createElement('ul');
+            answerEl.className = "question-list"
+        groupEl.appendChild(answerEl);
+
+          for (var i = 0; i < testObj.answer.length; i++) {
+    var answerListEl = document.createElement("li");
+        answerEl.appendChild(answerListEl);
+
+    var answerBtn = document.createElement('button')
+    answerBtn.textContent = testObj.answer[i];
+    answerBtn.className = "btn";
+    answerBtn.type = "submit"
+    answerListEl.appendChild(answerBtn);
+
+    }
 }
-
-
-
-var testFunc = function(question1) {
-
-    var questionEl = document.createElement("p");
-        questionEl.textContent = testObj.question;
-        questionInputEl.appendChild(questionEl);
-
-    for (var i = 0; i < testObj.answer.length; i++) {
-        var answerEl = document.createElement("li");
-            answerEl.textContent = testObj.answer[i];
-            answerEl.className = "btn"
-            answerInputEl.appendChild(answerEl);
-        }
-}   
-
-// testFunc()
+        
+        
     
+
+//     var createAnswerEl = function(testObj) {
+//         var ansGroupeEl = document.createElement('div');
+//         ansGroupEl.document.createElement('ul')
+//         questionGroupEl.appendChild(ansGroupeEl);
+
+
+
+
+//     for (var i = 0; i < testObj.answer.length; i++) {
+//     var answerEl = document.createElement("li");
+//         answerEl.textContent = testObj.answer[i];
+//         answerEl.className = "btn"
+//         answerInputEl.appendChild(answerEl);
+//     }
+//     }
+// }
+
+
+    
+
+
+
+
+
+
+        // var questionEl = document.createElement("p");
+        //     questionEl.textContent = testObj.question;
+        //     questionInputEl.appendChild(questionEl);
+
+        // for (var i = 0; i < testObj.answer.length; i++) {
+        //     var answerEl = document.createElement("li");
+        //         answerEl.textContent = testObj.answer[i];
+        //         answerEl.className = "btn"
+        //         answerInputEl.appendChild(answerEl);
+        //     }
+    
+ 
 question1()
+    
+
+
+
+var question2 = function () {
+    var testObj = {
+        question: "What day is it?",
+        answer: ["Monday", "tuesday", "wednesyday", "Thursday"]   
+    }
+      console.log(testObj);
+      testFunc(testObj)
+
+}     
 
 
 
 
-var buttonEl = document.querySelector("#question2-id", "#question1-id");
 
 
 
